f5c77f799da7f01ac1dfa6caf75f8e2c9e516af9
andersbuck <buckles.anderson@gmail.com>
2024-01-03 19:51:48 -0500

    Refactor to provide tools account number through cmd parameter

--- target-account/app.ts
+++ target-account/app.ts
@@ -1,28 +1,12 @@
-#!/usr/bin/env node
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: MIT-0
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
 import { CrossAccountRolesStack } from './src/cdk-stack';
 import fs from 'fs';
 
 // Read parameter file
-const params = JSON.parse(fs.readFileSync('src/cdk-stack-param.json', 'utf-8'))
+const params = JSON.parse(fs.readFileSync('src/cdk-stack-param.json', 'utf-8'));
+// Insert the provided Tools account number
+const toolsAccountUserArn = params.TOOLS_ACCOUNT_USER_ARN.replace('${TOOLS_ACCOUNT_NUMBER}', process.env.TOOLS_ACCOUNT_NUMBER);
 
 // Create App
 const app = new App();
@@ -30,5 +14,5 @@ const app = new App();
 new CrossAccountRolesStack(app, 'CrossAccountRolesStack', {
   description: 'Creates Cross Account Role and Cloudformation Execution Roles',
   stackName: 'cf-CrossAccountRolesStack',
-  toolsAccountUserArn: params.TOOLS_ACCOUNT_USER_ARN
+  toolsAccountUserArn: toolsAccountUserArn
 });
