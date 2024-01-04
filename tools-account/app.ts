3c83f8f0bb03f90d226e875a645300c448c83cc7
andersbuck <buckles.anderson@gmail.com>
2024-01-03 20:01:15 -0500

    Refactor to provide target account number through cmd parameter

--- tools-account/app.ts
+++ tools-account/app.ts
@@ -1,11 +1,12 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
 import { GitActionDeploymentUserStack } from './src/cdk-stack';
 import fs from 'fs';
 
 // Read parameter file
-const params = JSON.parse(fs.readFileSync('src/cdk-stack-param.json', 'utf-8'))
+const params = JSON.parse(fs.readFileSync('src/cdk-stack-param.json', 'utf-8'));
+// Insert the provided Target account number
+const crossAccountRoleArn = params.CROSS_ACCOUNT_ROLE_ARN.replace('${TARGET_ACCOUNT_NUMBER}', process.env.TARGET_ACCOUNT_NUMBER);
 
 // Create App
 const app = new App();
@@ -13,5 +14,5 @@ const app = new App();
 new GitActionDeploymentUserStack(app, 'GitActionDeploymentUserStack', {
   description: 'Creates Cross Account Role and Cloudformation Execution Roles',
   stackName: 'cf-GitActionDeploymentUserStack',
-  crossAccountRoleArn: params.CROSS_ACCOUNT_ROLE_ARN
+  crossAccountRoleArn: crossAccountRoleArn
 });
